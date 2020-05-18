c9a0c86e2f95755ab206765c58617aab4a889802
MihutSorin <mihut.sorin@yahoo.com>
2020-05-18 14:32:52 +0300

    add clasa names

---
+++ Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names nume = new Names();
+            nume.Print();
+            nume.Add("carmen ");
+            nume.Add("natalia teodora mihut");
+            nume.Print();
+            Console.ReadKey();
+        }
+    }
+}
