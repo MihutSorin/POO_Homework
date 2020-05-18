3a22bb794ffc09027a3cbcb79f13332c6f45a895
MihutSorin <mihut.sorin@yahoo.com>
2020-05-18 14:57:57 +0300

    create clasa Balls

---
+++ BBG/Balls.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBG
+{
+    public  class Balls
+    {
+        private int x, y;
+        private int raza;
+        private int pozitie;
+        private string type;
+       
+        public Balls(int x, int y, int raza,int pozitie,string type)
+        {
+            this.x = x;
+            this.y = y;
+            this.raza = raza;
+            this.pozitie = pozitie;
+            this.type = type;
+
+        }
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public int R
+        {
+            get
+            {
+                return raza;
+            }
+            set
+            {
+                raza = value;
+            }
+        }
+        public int poz
+        {
+            get
+            {
+                return pozitie;
+            }
+            set
+            {
+                pozitie = value;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+    }
+}
---
+++ BBG/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Balls b1 = new Balls(2, 4, 5, 6,"regular");
+            Balls b2 = new Balls(2, 2, 2, 2, "monster");
+            Balls b3 = new Balls(2, 2, 2, 2, "repelent");
+            Console.WriteLine(b2.Color);
+
+            Console.ReadLine();
+        }
+    }
+}
