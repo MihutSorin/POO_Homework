dfe65f1fbb01c1f785dcb0a361eaea7f0f711e57
MihutSorin <mihut.sorin@yahoo.com>
2020-05-20 13:05:34 +0300

    add stiva.cs

--- clasa stiva1/Program.cs
+++ clasa stiva1/Program.cs
@@ -10,11 +10,15 @@ namespace clasa_stiva1
     {
         static void Main(string[] args)
         {
-            Stiva stiva = new Stiva();
-             stiva.Push(1234);
-                int numar = (int)stiva.Pop();
-                Console.WriteLine(numar);
-                Console.ReadLine();
+           Stiva<int> s1 = new Stiva<int>(); //creaza o stiva de intregi
+                s1.Push(1234); //adauga un intreg in stiva t
+            int numar = s1.Pop(); //extrage un element din stiva, nu mai estenecesar cast
+            Console.WriteLine("Valoarea extrasa din stiva de intregi este{0}",numar);
+            Stiva<string> s2 = new Stiva<string>(); //creaza o stiva de stringuri
+            s2.Push("exemplu"); //adauga un string in stiva
+                string s = s2.Pop(); //extrage un string din stiva
+             Console.WriteLine("Valoarea extrasa din stiva de stringuri este'{0}'",s);
+            Console.ReadLine();
 
         }
     }
--- clasa stiva1/Stiva.cs
+++ clasa stiva1/Stiva.cs
@@ -1,30 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace clasa_stiva1
 {
-    class Stiva
+    class Stiva<T>
     {
-        private object[] m_ItemsArray;
+        private T[] m_ItemsArray;
+
         private int m_Index = 0;
         public const int MAX_SIZE = 100;
-        public Stiva() { m_ItemsArray = new object[MAX_SIZE]; }
-        public Object Pop()
+        public Stiva() { m_ItemsArray = new T[MAX_SIZE]; }
+        public T Pop()
         {
             if (m_Index == 0)
                 throw new InvalidOperationException("Nu putem extrage un element dintr - o stiva vida.");
-        return m_ItemsArray[--m_Index];
+            return m_ItemsArray[--m_Index];
         }
         public void Push(Object item)
         {
             if (m_Index == MAX_SIZE)
                 throw new StackOverflowException("Nu se poate adauga un element:stiva este plina.");
-               
-                m_ItemsArray[m_Index++] = item;
-        }
 
+            m_ItemsArray[m_Index++] = (T)item;
+        }
     }
 }
