86ef77c25709959239870e7391fc92cc4fe591f7
MihutSorin <mihut.sorin@yahoo.com>
2020-05-31 21:20:46 +0300

    Create Program.cs

---
+++ Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public delegate void iteratie(int i);     // declarare tip delegat atasat evenimentului
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher pb = new Publisher();
+            SubscriberA SBa = new SubscriberA();
+            SubscriberB SBb = new SubscriberB();
+
+            pb.eveniment += SBa.metodaClasaSubscriber;
+            pb.eveniment += SBb.metodaClasaSubscriber;
+
+            pb.DeclansareEveniment();
+
+            Console.ReadKey();
+
+        }
+    }
+}
