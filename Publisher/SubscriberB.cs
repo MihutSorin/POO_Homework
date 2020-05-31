565a26130ef72aab05f9e83bef72c3f78bf17a36
MihutSorin <mihut.sorin@yahoo.com>
2020-05-31 21:21:14 +0300

    Create SubscriberB.cs

---
+++ Publisher/SubscriberB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+   public class SubscriberB
+    {
+        public void metodaClasaSubscriber(int x)
+        {
+            Console.WriteLine("Ne aflam in corpul metodei clasei subscriberB la iteratia: {0}", x);
+        }
+
+    }
+}
