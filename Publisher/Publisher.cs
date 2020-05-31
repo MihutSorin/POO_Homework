6932ed336a6edc3efc43b230f60b276b74797570
MihutSorin <mihut.sorin@yahoo.com>
2020-05-31 21:20:58 +0300

    Create Publisher.cs

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+   public class Publisher
+    {
+        public event iteratie eveniment;
+
+        public void DeclansareEveniment()
+        {
+            for (int i = 1; i <= 10; i = i + 2)
+            {
+                if (eveniment != null)     // verificam existenta metodelor subscriber
+                    eveniment(i);          // declansam evenimentul
+            }
+        }
+
+    }
+}
