889886b1725828d7c9a3bcf65ff171ff4de9d3ee
MihutSorin <mihut.sorin@yahoo.com>
2020-06-03 14:38:13 +0300

    Update Program.cs

--- Angajat/Program.cs
+++ Angajat/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Angajat
 {
     class Program
     {
+        public static List<Angajat> _angajat = new List<Angajat>();
         static void Main(string[] args)
         {
+            _angajat = new List<Angajat>();
+
+            TextReader dataLoad = new StreamReader(@"E:\POO_Homework\Angajat\DataBase.txt");
+            string buffer = dataLoad.ReadToEnd();
+            Console.WriteLine(buffer);
+            
+            Console.ReadKey();
         }
     }
 }
