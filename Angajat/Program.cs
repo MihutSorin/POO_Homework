49380d6d245c6231d051993c14b72bd9d5da81b3
MihutSorin <mihut.sorin@yahoo.com>
2020-06-04 13:19:00 +0300

    Update Program.cs

--- Angajat/Program.cs
+++ Angajat/Program.cs
@@ -12,12 +12,18 @@ namespace Angajat
         public static List<Angajat> _angajat = new List<Angajat>();
         static void Main(string[] args)
         {
-            _angajat = new List<Angajat>();
+          //  _angajat = new List<Angajat>();
 
-            TextReader dataLoad = new StreamReader(@"E:\POO_Homework\Angajat\DataBase.txt");
-            string buffer = dataLoad.ReadToEnd();
-            Console.WriteLine(buffer);
-            
+            //TextReader dataLoad = new StreamReader(@"E:\POO_Homework\Angajat\DataBase.txt");
+            //string buffer = dataLoad.ReadToEnd();
+            //Console.WriteLine(buffer);
+            string filepath = @"E:\POO_Homework\Angajat\DataBase.txt";
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllLines(filepath, lines);
             Console.ReadKey();
         }
     }
