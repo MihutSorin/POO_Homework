851ede144e21bd5ffbade1d696ea1e5af2416080
MihutSorin <mihut.sorin@yahoo.com>
2020-06-04 13:19:08 +0300

    Create Program.cs

---
+++ Proiect Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names nume = new Names();
+            nume.Print();
+            nume.Add("carmen ");
+            nume.Add("natalia teodora ");
+            nume.Print();
+            Console.ReadKey();
+        }
+    }
+}
