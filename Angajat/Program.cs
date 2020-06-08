6e49922cec55f548d4350c9a5c6ddc21e649b0dd
MihutSorin <mihut.sorin@yahoo.com>
2020-06-08 11:29:33 +0300

    Update Program.cs

--- Angajat/Program.cs
+++ Angajat/Program.cs
@@ -9,31 +9,58 @@ namespace Angajat
 {
     class Program
     {
-        public static List<Angajat> _angajati = new List<Angajat>();
+        public static Angajati<Persoana> _angajati = new Angajati<Persoana>();
         static void Main(string[] args)
         {
            
             Load(@"..\..\DataBase.txt");
-            view();
+            _angajati.SortByName();
+            
+            LoadtoDB(@"E:\POO_Homework\Angajat\Databaseinfo.txt");
+            _angajati.Vechime();
+            LoadtoDBVechime(@"E:\POO_Homework\Angajat\DBVechime.txt");
             Console.ReadKey();
         }
         static void Load(string filename)
         {
-            TextReader dataload = new StreamReader(filename);
-            int n = int.Parse(dataload.ReadLine());
+            _angajati = new Angajati<Persoana>();
+            StreamReader dataLoad = new StreamReader(filename);
+            string line = dataLoad.ReadLine();
 
-            for (int i = 0; i < n; i++)
+            while (line != null)
             {
-                _angajati.Add(new Angajat(dataload.ReadLine()));
+                string[] date = line.Split(' ');
+                string nume = date[0];
+                string prenume = date[1];
+                string[] fullDate = date[2].Split('/');
+                DateTime data = new DateTime(int.Parse(fullDate[0]), int.Parse(fullDate[1]), int.Parse(fullDate[2]));
+
+                Persoana angajati = new Persoana(nume, prenume, data);
+                _angajati.Add(angajati);
+
+                line = dataLoad.ReadLine();
             }
         }
-        static void view()
+        private static void LoadtoDB(string filepath)
         {
-            foreach (Angajat ang in _angajati)
+            StreamWriter dataOutput = new StreamWriter(filepath);
+            foreach (Persoana angajat in _angajati.list)
             {
-                Console.WriteLine(ang.ToString());
+                dataOutput.WriteLine($"{angajat.nume} {angajat.prenume} ");
             }
+            dataOutput.Close();
         }
-       
+        private static void LoadtoDBVechime(string filepath)
+        {
+            StreamWriter dataOutput = new StreamWriter(filepath);
+            foreach (Persoana angajat in _angajati.list)
+            {
+                int vechime = (DateTime.Now.Year - angajat.data.Year) * 12 + (DateTime.Now.Month - angajat.data.Month);
+                dataOutput.WriteLine($"{angajat.nume} {angajat.prenume} :are o vechime de: " +
+                    $"{vechime / 12} ani and " +
+                    $"{vechime % 12} luni");
+            }
+            dataOutput.Close();
+        }
     }
 }
